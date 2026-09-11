dfc660b91ebfc0c4eb51f1c0fbfa658d541d20e3
YDDAMeta <yadhir@intern-mail.metabox.technology>
2026-09-11 10:29:26 +0400

    implement correction

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -54,7 +54,7 @@ export default function App() {
 
                              />
                          </View>
-                         <ListHeading title="All Subscription" />
+                         <ListHeading title="All Subscriptions" />
                      </>
 
                     )}
